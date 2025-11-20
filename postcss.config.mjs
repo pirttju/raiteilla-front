2279f56de3cb62c00884f32512e7f9f77d19c529
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2025-11-20 23:55:36 +0200

    Try to fix things

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    // Note: v4 uses this specific package, not 'tailwindcss'
     "@tailwindcss/postcss": {},
   },
 };
